961624e1185ac85c1d632eb6fdc196ab52d518ab
WangJian <wangjian@local.com>
2026-07-23 18:10:48 +0800

    修复背单词页面的输入框被遮挡的问题

--- app/src/main/java/com/example/english/ui/screen/WordScreen.kt
+++ app/src/main/java/com/example/english/ui/screen/WordScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -326,6 +327,7 @@ fun WordScreen(
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(innerPadding)
+                        .imePadding()
                 ) {
                     // Scrollable content
                     Column(
