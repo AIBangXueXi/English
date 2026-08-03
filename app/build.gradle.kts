20b5ffa65b325f5ca34ebe5744686ce48438fe2c
WangJian <wangjian@local.com>
2026-08-03 16:57:28 +0800

    增加版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.english"
         minSdk = 24
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.11.0"
+        versionCode = 12
+        versionName = "1.12.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
