35b3937cb9610eaf0ecfb19b5b2fd84280eb6e80
WangJian <wangjian@local.com>
2026-08-12 10:11:42 +0800

    恢复一下播放发音的形式

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.english"
         minSdk = 24
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.16.0"
+        versionCode = 18
+        versionName = "1.18.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
