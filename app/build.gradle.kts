0cae55f7f92b9ad60c4f89ddcf296c067ba3e118
WangJian <wangjian@local.com>
2026-08-27 16:46:28 +0800

    新版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.english"
         minSdk = 24
         targetSdk = 36
-        versionCode = 24
-        versionName = "1.24.0"
+        versionCode = 25
+        versionName = "1.25.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
