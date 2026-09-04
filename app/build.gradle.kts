18058acc00d51c0f99f96d5ac70ebd1bfc800479
WangJian <wangjian@local.com>
2026-09-04 14:52:04 +0800

    语音识别失败不要崩溃

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.english"
         minSdk = 24
         targetSdk = 36
-        versionCode = 28
-        versionName = "1.28.0"
+        versionCode = 29
+        versionName = "1.29.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
