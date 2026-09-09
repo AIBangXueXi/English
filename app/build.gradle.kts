25bb6f04697f288ca187c54f838055e62ba52d7b
WangJian <wangjian@local.com>
2026-09-09 20:06:06 +0800

    bump version to 1.47.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.english"
         minSdk = 24
         targetSdk = 36
-        versionCode = 46
-        versionName = "1.46.0"
+        versionCode = 47
+        versionName = "1.47.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
