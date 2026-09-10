3c238c85cde59bba85955cfbc0b64672e7025f9d
WangJian <wangjian@local.com>
2026-09-10 16:38:57 +0800

    bump version to 1.48.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.example.english"
         minSdk = 24
         targetSdk = 36
-        versionCode = 47
-        versionName = "1.47.0"
+        versionCode = 48
+        versionName = "1.48.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
