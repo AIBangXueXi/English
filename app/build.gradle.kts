9cee0ea20460ca8091f8e4157b9acafe0777840b
WangJian <wangjian@local.com>
2026-09-14 00:08:20 +0800

    bump version to 1.52.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.example.english"
         minSdk = 24
         targetSdk = 36
-        versionCode = 51
-        versionName = "1.51.0"
+        versionCode = 52
+        versionName = "1.52.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
